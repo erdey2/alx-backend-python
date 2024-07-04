7a7a5850c9cb6cc788057c6a5746f086de6ad780
erdey <erdeysyoum@gmail.com>
2024-07-04 05:44:30 -0700

    convert to tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""complex types module """
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, Union[int, float] -> Tuple[str, float]:
+    """ convert to tuple """
+    return (k, v**2)
