e49d8eb86b0ca00c0ae7618984933255bcacc6f5
erdey <erdeysyoum@gmail.com>
2024-07-04 02:01:38 -0700

    type annotation sum list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """type annotation list module """
+from typing import List
 
 
-def sum_list(input_list: list) -> float:
+def sum_list(input_list: List[float]) -> float:
     sum: float = 0
     for(i in input_list):
         sum += i
