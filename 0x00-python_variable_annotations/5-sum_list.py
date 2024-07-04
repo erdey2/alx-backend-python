34965fe3de3d8c6be4216b6e8a2a49b1715400e4
erdey <erdeysyoum@gmail.com>
2024-07-04 02:47:51 -0700

    typed annotation floor

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -5,7 +5,8 @@ from typing import List
 
 def sum_list(input_list: List[float]) -> float:
     sum: float = 0
-    for(i in input_list):
+    i: int
+    for i in input_list:
         sum += i
 
     return sum
