875375b351625141bfa8b887e5e5ebb839c30e60
erdey <erdeysyoum@gmail.com>
2024-07-08 08:00:14 -0700

    Asynchronous execution

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay: int = 10) -> float:
     """Runs after delay """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
